6bd77b84f0ea161f69fef56babbdbc4ee7ce64ab
Will Hurley <will@hurleybirdjr.com>
2025-11-12 00:47:47 +0000

    reworked for stereoscape

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,19 +7,12 @@ import (
 )
 
 type Config struct {
-	Environment          string            `json:"environment"`
-	GuildID              string            `json:"guild_id"`
-	LoungeChannelID      string            `json:"lounge_channel_id"`
-	FeedbackChannelID    string            `json:"feedback_channel_id"`
-	DemosChannelID       string            `json:"demos_channel_id"`
-	EventStageChannelId  string            `json:"event_stage_channel_id"`
-	EventStreamChannelId string            `json:"event_stream_channel_id"`
-	EventChatChannelId   string            `json:"event_chat_channel_id"`
-	HangoutRoomId        string            `json:"hangout_room_id"`
-	InstantBanChannelId  string            `json:"instant_ban_channel_id"`
-	YoutubeChannels      []YoutubeChannel  `json:"youtube_channels"`
-	Roles                map[string]string `json:"roles"`
-	Reactions            Reactions         `json:"reactions"`
+	Environment         string            `json:"environment"`
+	GuildID             string            `json:"guild_id"`
+	GeneralChannelID    string            `json:"general_channel_id"`
+	InstantBanChannelId string            `json:"instant_ban_channel_id"`
+	Roles               map[string]string `json:"roles"`
+	Reactions           Reactions         `json:"reactions"`
 }
 
 type Reactions struct {
@@ -30,11 +23,6 @@ type Reactions struct {
 type Reaction struct {
 	Emoji   *string `json:"emoji"`
 	Message *string `json:"message"`
-}
-
-type YoutubeChannel struct {
-	Name string `json:"name"`
-	Feed string `json:"feed"`
 }
 
 func (c *Config) Load() error {
